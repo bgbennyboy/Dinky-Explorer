13f2eafd5fa0c6192624e35b4fce0fdf594e4654
Bennyboy <bgbennyboy@users.noreply.github.com>
2022-11-11 20:50:56 +0000

    Bugfix release - audio wasnt working in the release build because ilmerge wasnt packaging in the fmod dll. Couldnt get it working so changed to Costura (https://github.com/Fody/Costura) for the merging.

--- ThimbleweedParkExplorer/Constants.cs
+++ ThimbleweedParkExplorer/Constants.cs
@@ -8,7 +8,7 @@ namespace ThimbleweedParkExplorer
     public static class Constants
     {
         // Change the version in Properties/AssemblyInfo.cs 
-        public static string Version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString(2);
+        public static string Version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString(3);
         public const string URL = "http://quickandeasysoftware.net";
         public const string ProgName = "Dinky Explorer";
         public const string AllFiles_ContextMenu = "All Files";
